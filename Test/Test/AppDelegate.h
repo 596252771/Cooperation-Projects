14788c12e8e2a95dee6961dc3ee95e945aa34711
程洋 <chengyang@chengyangdemacbook-pro.local>
2016-07-28 17:22:06 +0800

    创建

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by 程洋 on 16/7/28.
+//  Copyright © 2016年 程洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by 程洋 on 16/7/28.
+//  Copyright © 2016年 程洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
