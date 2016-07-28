ffcc5ccfffda7b49b17cb75b0d9fbd1181b4f58a
程洋 <chengyang@chengyangdemacbook-pro.local>
2016-07-28 17:26:09 +0800

    添加person类

---
+++ Test/Test/Preson.h
@@ -0,0 +1,13 @@
+//
+//  Preson.h
+//  Test
+//
+//  Created by 程洋 on 16/7/28.
+//  Copyright © 2016年 程洋. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Preson : NSObject
+
+@end
---
+++ Test/Test/Preson.m
@@ -0,0 +1,13 @@
+//
+//  Preson.m
+//  Test
+//
+//  Created by 程洋 on 16/7/28.
+//  Copyright © 2016年 程洋. All rights reserved.
+//
+
+#import "Preson.h"
+
+@implementation Preson
+
+@end
